16f96f0acd035a0ad50a351c1151747b8341ff19
DindaKartika <dkartika0887@gmail.com>
2019-02-14 17:14:00 +0700

    Upload Model + Database

--- berita/admin.py
+++ berita/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import Kategori, Berita
 # Register your models here.
+
+my_model = [Kategori, Berita]
+admin.site.register(my_model)
--- member/models.py
+++ member/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import CharField
+from django.db.models import TextField
+from django.db.models import ImageField
+from django.utils import timezone
 
 # Create your models here.
+class Member(models.Model):
+    user_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 100, default="")
+    password = models.CharField(max_length = 25)
