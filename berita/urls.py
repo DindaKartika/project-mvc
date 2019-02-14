a93261c68a2a933870c300a02b244b89d059b82d
DindaKartika <dkartika0887@gmail.com>
2019-02-14 20:16:41 +0700

    update home

--- berita/urls.py
+++ berita/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.base, name='base'),
-   path('h', views.home, name='home'),
+   path('', views.home, name='home'),
    path('index', views.index, name='index'),
    path('foto', views.foto, name='foto'),
    path('berita/<int:id>', views.hlm_berita, name='hlmberita')
